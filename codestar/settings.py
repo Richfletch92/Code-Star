85770e675b6d4db5766b81f848bffb0ccb3074e9
Richard Fletcher <richfletch92@gmail.com>
2025-01-15 16:44:27 +0000

    add previous page control to blog posts

--- codestar/settings.py
+++ codestar/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_summernote',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'blog', # Add the blog app to the list of installed apps
 ]
 
@@ -134,9 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
