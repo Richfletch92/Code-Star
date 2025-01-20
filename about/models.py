f7d3aa4a5604ed6ffe6e6d46832f74320f9009c1
Richard Fletcher <richfletch92@gmail.com>
2025-01-20 12:08:21 +0000

    add cloudinary function to profile image on about page

--- about/models.py
+++ about/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
 class About(models.Model):
     title = models.CharField(max_length=200, unique=True)
     content = models.TextField()
-    updated_on = models.DateTimeField(auto_now= True)
+    updated_on = CloudinaryField('image', default='placeholder')
     
     def __str__(self):
         return self.title
