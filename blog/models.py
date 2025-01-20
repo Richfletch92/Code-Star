42ad5d8286a6ba987ae4e930a8857bdecfe15954
Richard Fletcher <richfletch92@gmail.com>
2025-01-20 11:42:01 +0000

    add cloudinary function

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 STATUS = (
     (0,"Draft"),
@@ -11,6 +12,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
@@ -18,7 +20,7 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now= True)
 
     class Meta:
-        ordering = ["created_on"]
+        ordering = ["-created_on"]
 
     def __str__(self):
         return f"{self.title} | written by {self.author}"
@@ -34,4 +36,4 @@ class Comment(models.Model):
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment {self.body} written by {self.author}"
+        return f"Comment {self.body} by {self.author}"
--- codestar/settings.py
+++ codestar/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote', # Add the django_summernote app to the list of installed apps
+    'cloudinary', # Add the cloudinary app to the list of installed apps
     'blog', # Add the blog app to the list of installed apps
     'about', # Add the about app to the list of installed apps
 ]
